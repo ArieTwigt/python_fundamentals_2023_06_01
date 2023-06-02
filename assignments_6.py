682572acb700c095ddb93a22eb763ca1df35fa80
Arie Twigt <twigt.arie@gmail.com>
2023-06-02 14:19:20 +0200

    added exercise 6

--- assignments_6.py
+++ assignments_6.py
@@ -30,7 +30,36 @@ for name in names_list:
 print(new_names)
 
 
+###### Assignment 2 - Create a loop that prints the name of the day for the following 10 days
+#%%
+from datetime import date, timedelta
+
+today_date = date.today()
+
+num_days = 10
+
+days_names_list = []
+
+for num_day in range(1, num_days + 1):
+    new_date = today_date + timedelta(days=num_day)
+    day_name = new_date.strftime("%A")
+
+    if day_name not in ['Saturday', 'Sunday']:
+        days_names_list.append(day_name)
+
 # %%
-naam += "T"
-print(naam)
+today_date = date.today()
+
+days_count = 0
+
+days_names_list = []
+
+
+while days_count < 10:
+        today_date = today_date + timedelta(days=1)
+        day_name = today_date.strftime("%A")
+
+        if day_name not in ['Saturday', 'Sunday']:
+            days_names_list.append(day_name)
+            days_count += 1
 # %%
